7a409f2a05b9dec25dea9d27a022bce55e1f9c05
Mert <mert.pekduzgun@gmail.com>
2019-11-01 20:54:19 +0300

    [TASK] Initial Commit

---
+++ Cram/Cram/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Cram
+//
+//  Created by Mert on 1.11.2019.
+//  Copyright © 2019 Mert. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
